2f3c39a0c1aeab45b89558af6bef52ccb7c4fa97
RFilipovic <rene.filipovic03@gmail.com>
2025-11-02 22:30:45 +0100

    added carousel logic

--- main.c
+++ main.c
@@ -24,6 +24,8 @@
 #define MSG_USTANI 4
 #define MSG_RIDE 5
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 pthread_t thread;
 PriorityQueue pq;
 
@@ -121,10 +123,12 @@ void *visitor_thread(void *args) {
                 msg = receive_message(i, data->id);
                 
                 if(msg.type != -1) {  
-
-                    Ci++;
+                    Ci = MAX(Ci, msg.Tm) + 1;
+                    
                     if(msg.type == MSG_REQUEST) {
-
+                        printf("Posjetitelj %d obrađuje zahtjev od %d (Tm=%d)\n", 
+                               data->id, msg.sender_id, msg.Tm);
+                        
                         enqueue(&pq, &msg);
 
                         if (taken == 1) {
@@ -132,8 +136,7 @@ void *visitor_thread(void *args) {
                             printf("Posjetitelj %d je u kritičnoj sekciji — odgađa odgovor za posjetitelja %d\n",
                                 data->id, msg.sender_id);
                         } else {
-                            if(findByProcessNum(&pq, data->id) == -1 || 
-                            msg.Tm < Ci) {
+                            if(findByProcessNum(&pq, data->id) == -1 || msg.Tm < Ci) {
                                 Message reply;
                                 reply.type = MSG_REPLY;
                                 reply.sender_id = data->id;
@@ -141,8 +144,6 @@ void *visitor_thread(void *args) {
                                 reply.wants_ride = 0;
                                 
                                 send_message(data->id, msg.sender_id, &reply);
-                                printf("Posjetitelj %d šalje odgovor posjetitelju %d\n", 
-                                    data->id, msg.sender_id);
                             }
                         }
                     }
@@ -157,31 +158,35 @@ void *visitor_thread(void *args) {
 
         msg = receive_message(CAROUSEL_ID, data->id);
         if(msg.type != -1) {
-            Ci++;
+            Ci = MAX(Ci, msg.Tm) + 1;
+            
             if(msg.type == MSG_SJEDNI) { 
                 rcvd_replies = 0;  
-                printf("Posjetitelj %d primio poruku da sjedne\n", data->id);
+                printf("Posjetitelj %d sjeo na vrtuljak\n", data->id);
+                
                 Message result;
                 dequeue(&pq, &result);
 
                 Message confirm;
                 confirm.type = MSG_REPLY;
                 confirm.sender_id = data->id;
-                confirm.Tm = msg.Tm;
+                confirm.Tm = Ci;
                 confirm.wants_ride = 1;
                 send_message(data->id, CAROUSEL_ID, &confirm);
-                printf("Posjetitelj %d potvrdio sjedanje\n", data->id);
+                
                 taken = 1;
                 sleep(3);
             }
             else if(msg.type == MSG_USTANI) { 
-                printf("Posjetitelj %d primio poruku da ustane\n", data->id);
+                printf("Posjetitelj %d ustao s vrtuljka\n", data->id);
+                
                 Message confirm;
                 confirm.type = MSG_REPLY;
                 confirm.sender_id = data->id;
-                confirm.Tm = msg.Tm;
+                confirm.Tm = Ci;
                 confirm.wants_ride = 0;
                 send_message(data->id, CAROUSEL_ID, &confirm);
+                
                 taken = 0;
                 rides++;
 
@@ -193,7 +198,7 @@ void *visitor_thread(void *args) {
                         reply.Tm = Ci;
                         reply.wants_ride = 0;
                         send_message(data->id, i, &reply);
-                        printf("Posjetitelj %d sada šalje odgođeni odgovor posjetitelju %d\n",
+                        printf("Posjetitelj %d šalje odgođeni odgovor posjetitelju %d\n",
                             data->id, i);
                         deferred_replies[i] = 0;
                     }
@@ -222,21 +227,22 @@ void visitor_process(int id) {
         if(findByProcessNum(&pq, id) == -1 && taken == 0) {
             Message msg;
             msg.Tm = Ci;
-            msg.type = 1;
+            msg.type = MSG_REQUEST;
             msg.sender_id = id;
             msg.wants_ride = 1;
 
             enqueue(&pq, &msg);
 
+            printf("Posjetitelj %d šalje zahtjeve svim posjetiteljima (Tm=%d)\n", id, Ci);
             for(int i = 0; i < NUM_VISITORS; i++) {
                 if(i != id) {
                     send_message(id, i, &msg);
-                    printf("Posjetitelj %d šalje zahtjev posjetitelju %d\n", id, i);
                 }
             }
         }
         else if(rcvd_replies == NUM_VISITORS - 1){
-
+            printf("Posjetitelj %d primio sve odgovore, šalje zahtjev vrtuljku\n", id);
+            
             Message carousel_msg;
             carousel_msg.type = MSG_RIDE;
             carousel_msg.sender_id = id;
@@ -244,38 +250,135 @@ void visitor_process(int id) {
             carousel_msg.wants_ride = 1;
             
             send_message(id, CAROUSEL_ID, &carousel_msg);
-            printf("Posjetitelj %d šalje zahtjev vrtuljku\n", id);
             
             rcvd_replies = 0;
         }
-
     }
     
-    printf("Posjetitelj %d završio.\n", id);
+    printf("Posjetitelj %d završio s %d vožnji.\n", id, rides);
     exit(0);
 }
-
-void *carousel_thread(void *args){
-
-    return NULL;
-}
-
-//typedef struct {
-//    int type;           
-//    int sender_id;      
-//    int Tm;      
-//    int wants_ride;     
-//} Message;
 
 void carousel_process() {
     int active_visitors = NUM_VISITORS;
     int Ci = rand() % 10;
     Message msg;
+    int waiting_visitors = 0;
+    int riding_visitors = 0;
+    int visitor_rides[NUM_VISITORS] = {0};
+    int visitor_states[NUM_VISITORS] = {0}; 
+    
+    printf("Vrtuljak: Pokrećem se s %d posjetitelja\n", active_visitors);
     
     while (active_visitors > 0) {
+        for (int i = 0; i < NUM_VISITORS; i++) {
+            msg = receive_message(i, CAROUSEL_ID);
+            
+            if (msg.type != -1) {
+                Ci = MAX(Ci, msg.Tm) + 1;
+                
+                if (msg.type == MSG_RIDE) {
+                    if (visitor_states[msg.sender_id] == 2) {
+                        continue;
+                    }
+                    
+                    if (visitor_states[msg.sender_id] == 0) {
+                        waiting_visitors++;
+                        printf("Vrtuljak: Posjetitelj %d u redu za čekanje (%d/%d)\n", 
+                               msg.sender_id, waiting_visitors, CAROUSEL_SEATS);
+                    }
+                    
+                } else if (msg.type == MSG_REPLY && msg.wants_ride == 1) {
+                    riding_visitors++;
+                    visitor_states[msg.sender_id] = 1;
+                    printf("Vrtuljak: Posjetitelj %d sjeo (%d/%d sjelo)\n", 
+                           msg.sender_id, riding_visitors, CAROUSEL_SEATS);
+                    
+                } else if (msg.type == MSG_REPLY && msg.wants_ride == 0) {
+                    riding_visitors--;
+                    visitor_states[msg.sender_id] = 0;
+                    visitor_rides[msg.sender_id]++;
+                    
+                    printf("Vrtuljak: Posjetitelj %d ustao (%d vožnji)\n", 
+                           msg.sender_id, visitor_rides[msg.sender_id]);
+                    
+                    if (visitor_rides[msg.sender_id] >= RIDES_PER_VISITOR) {
+                        printf("Vrtuljak: Posjetitelj %d ZAVRŠIO\n", msg.sender_id);
+                        visitor_states[msg.sender_id] = 2;
+                        active_visitors--;
+                        printf("Vrtuljak: Preostalo aktivnih: %d\n", active_visitors);
+                    }
+                }
+            }
+        }
+
+        if (waiting_visitors >= CAROUSEL_SEATS && riding_visitors == 0) {
+            printf("Vrtuljak: POKRETANJE vožnje (%d čeka)\n", waiting_visitors);
+
+            int seated = 0;
+            for (int i = 0; i < NUM_VISITORS && seated < CAROUSEL_SEATS; i++) {
+                if (visitor_states[i] == 0 && waiting_visitors > 0) {
+                    Message sit_msg;
+                    sit_msg.type = MSG_SJEDNI;
+                    sit_msg.sender_id = CAROUSEL_ID;
+                    sit_msg.Tm = ++Ci;
+                    sit_msg.wants_ride = 0;
+                    
+                    send_message(CAROUSEL_ID, i, &sit_msg);
+                    
+                    waiting_visitors--;
+                    seated++;
+                }
+            }
+            
+            int confirmations = 0;
+            int timeout = 0;
+            while (confirmations < CAROUSEL_SEATS && timeout < 50) {
+                for (int i = 0; i < NUM_VISITORS; i++) {
+                    msg = receive_message(i, CAROUSEL_ID);
+                    if (msg.type != -1 && msg.type == MSG_REPLY && msg.wants_ride == 1) {
+                        confirmations++;
+                    }
+                }
+                usleep(100000); 
+                timeout++;
+            }
+            
+            if (confirmations == CAROUSEL_SEATS) {
+                printf("Vrtuljak: VOŽNJA ZAPOČETA\n");
+
+                int ride_duration = (rand() % 2001) + 1000;
+                usleep(ride_duration * 1000);
+                
+                printf("Vrtuljak: VOŽNJA ZAVRŠENA\n");
+
+                for (int i = 0; i < NUM_VISITORS; i++) {
+                    if (visitor_states[i] == 1) {
+                        Message stand_msg;
+                        stand_msg.type = MSG_USTANI;
+                        stand_msg.sender_id = CAROUSEL_ID;
+                        stand_msg.Tm = ++Ci;
+                        stand_msg.wants_ride = 0;
+                        
+                        send_message(CAROUSEL_ID, i, &stand_msg);
+                    }
+                }
+            }
+        }
         
+        usleep(100000); 
     }
     
+    printf("Vrtuljak je ZAVRŠIO S RADOM\n");
+    
+    // Čekaj da svi procesi završe
+    for (int i = 0; i < NUM_VISITORS; i++) {
+        wait(NULL);
+    }
+    
+    // Počisti cijevi
+    system("rm -f /tmp/pipe_*");
+    exit(0);
 }
 
 void signal_handler(int sig) {
