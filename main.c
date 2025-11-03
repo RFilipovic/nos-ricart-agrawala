9c7160d2b889572c7d8b8deaa00f9c43fd05d239
rene <rene.filipovic03@gmail.com>
2025-11-03 11:42:22 +0100

    final

--- main.c
+++ main.c
@@ -90,7 +90,38 @@ void create_all_pipes() {
     }
 }
 
+// --- Pomoćna funkcija za ispis tipa poruke ---
+const char* msg_type_to_string(int type) {
+    switch(type) {
+        case MSG_REQUEST: return "zahtjev (R-A)";
+        case MSG_REPLY:   return "odgovor (R-A)";
+        case MSG_SJEDNI:  return "sjedni";
+        case MSG_USTANI:  return "ustani";
+        case MSG_RIDE:    return "zahtjev (Vrtuljak)";
+        case MSG_DONE:    return "gotov";
+        default:          return "NEPOZNAT";
+    }
+}
+
 void send_message(int from, int to, Message *msg) {
+    // *** NOVI ISPIS ZA SLANJE ***
+    // Formatiranje: %-2d (za ID) i %-18s (za tip poruke) osigurava lijepo poravnanje
+    if (from == CAROUSEL_ID) {
+        // Vrtuljak šalje
+        printf("Vrtuljak  : Šaljem %-18s -> Posjetitelju %d (Tm=%d)\n", 
+               msg_type_to_string(msg->type), to, msg->Tm);
+    } else {
+        // Posjetitelj šalje
+        if (to == CAROUSEL_ID) {
+            printf("Posjetitelj %-2d: Šaljem %-18s -> Vrtuljku    (Tm=%d)\n", 
+                   from, msg_type_to_string(msg->type), msg->Tm);
+        } else {
+            printf("Posjetitelj %-2d: Šaljem %-18s -> Posjetitelju %d (Tm=%d)\n", 
+                   from, msg_type_to_string(msg->type), to, msg->Tm);
+        }
+    }
+    // *** KRAJ NOVOG ISPISA ***
+
     char pipe_name[50];
     get_pipe_name(pipe_name, from, to);
     int fd = open(pipe_name, O_WRONLY); 
@@ -119,40 +150,39 @@ void *visitor_thread(void *args) {
                 if (bytes_read > 0) { // Imamo poruku!
                     Ci = MAX(Ci, msg.Tm) + 1;
                     
+                    // *** NOVI ISPIS ZA PRIMANJE ***
+                    printf("Posjetitelj %-2d: Primio %-18s <- Posjetitelja %d (Tm=%d)\n", 
+                           data->id, msg_type_to_string(msg.type), msg.sender_id, msg.Tm);
+                    // *** KRAJ NOVOG ISPISA ***
+
                     if(msg.type == MSG_REQUEST) {
-                        printf("Posjetitelj %d: Primio R-A ZAHTJEV od %d (Tm=%d)\n", 
-                               data->id, msg.sender_id, msg.Tm);
-                        
+                        // OBRISAN dupli printf
                         enqueue(&pq, &msg); 
                         int my_req_index = findByProcessNum(&pq, data->id);
 
-                        // *** ISPRAVNA LOGIKA ODBIJANJA ***
-                        // Odbij ako:
-                        // 1. Si TRENUTNO na vožnji (taken == 1)
-                        // 2. Ili ako si U REDU za K.O. i imaš prednost (my_req_index != -1, i tvoj Tm je manji)
                         if (taken == 1) {
                             deferred_replies[msg.sender_id] = 1;
-                            printf("Posjetitelj %d: (Na vožnji) Odgađam odgovor za %d\n", data->id, msg.sender_id);
+                            printf("Posjetitelj %d: (Stanje: Na vožnji) Odgađam odgovor za %d\n", data->id, msg.sender_id);
                         } else if (my_req_index == -1) {
-                            // Ne tražim K.O. i nisam na vožnji, odgovori odmah
                             Message reply = {MSG_REPLY, data->id, Ci, 0};
                             send_message(data->id, msg.sender_id, &reply);
                         } else {
-                            // Nisam na vožnji, ali tražim K.O., provjeri R-A
                             Message my_req = pq.messages[my_req_index]; 
                             if (msg.Tm < my_req.Tm || (msg.Tm == my_req.Tm && msg.sender_id < data->id)) {
                                 Message reply = {MSG_REPLY, data->id, Ci, 0};
                                 send_message(data->id, msg.sender_id, &reply);
                             } else {
                                 deferred_replies[msg.sender_id] = 1;
-                                printf("Posjetitelj %d: (Moj Tm niži) Odgađam odgovor za %d\n", data->id, msg.sender_id);
+                                printf("Posjetitelj %d: (Stanje: Moj Tm niži) Odgađam odgovor za %d\n", data->id, msg.sender_id);
                             }
                         }
                     }
                     else if(msg.type == MSG_REPLY) {
+                        // OBRISAN dupli printf
                         rcvd_replies++;
-                        printf("Posjetitelj %d: Primio R-A ODGOVOR od %d (%d/%d)\n", 
-                               data->id, msg.sender_id, rcvd_replies, NUM_VISITORS - 1);
+                        // Ovaj ispis je koristan da vidimo kad je proces spreman ući u K.O.
+                        printf("Posjetitelj %-2d: (Brojač odgovora: %d/%d)\n", 
+                               data->id, rcvd_replies, NUM_VISITORS - 1);
                     }
                 } else if (bytes_read == 0) { 
                     close(data->read_fds[i]);
@@ -173,24 +203,28 @@ void *visitor_thread(void *args) {
             if (bytes_read > 0) {
                 Ci = MAX(Ci, msg.Tm) + 1;
                 
+                // *** NOVI ISPIS ZA PRIMANJE ***
+                printf("Posjetitelj %-2d: Primio %-18s <- Vrtuljka    (Tm=%d)\n", 
+                       data->id, msg_type_to_string(msg.type), msg.Tm);
+                // *** KRAJ NOVOG ISPISA ***
+
                 if(msg.type == MSG_SJEDNI) { 
-                    printf("Posjetitelj %d: Sjeo na vrtuljak\n", data->id);
-                    taken = 1; // Javi glavnoj petlji da može nastaviti
+                    printf("Posjetitelj %d: Sjeo.\n", data->id); // Logiramo akciju
+                    taken = 1; 
                 }
                 else if(msg.type == MSG_USTANI) { 
-                    printf("Posjetitelj %d: Ustao s vrtuljka\n", data->id);
-                    taken = 0; // Javi glavnoj petlji da je vožnja gotova
-                    rides++;   // Povećaj brojač vožnji
+                    printf("Posjetitelj %d: Ustao.\n", data->id); // Logiramo akciju
+                    taken = 0; 
+                    rides++;   
                     
-                    // *** POPRAVAK: Šalji odgođene odgovore NAKON vožnje ***
-                    // (Logika koja je nedostajala i uzrokovala zadnji deadlock)
                     printf("Posjetitelj %d: Sišao, šaljem odgođene R-A odgovore...\n", data->id);
-                    for (i = 0; i < NUM_VISITORS; i++) { // Ponovno koristi 'i'
+                    for (i = 0; i < NUM_VISITORS; i++) { 
                         if (deferred_replies[i]) {
-                            Ci++; // Uskladimo sat
+                            Ci++; 
                             Message reply = {MSG_REPLY, data->id, Ci, 0};
                             send_message(data->id, i, &reply);
-                            printf("Posjetitelj %d: Šaljem odgođeni R-A odgovor za %d\n", data->id, i);
+                            // Ovaj ispis brišemo, jer ga sad radi send_message
+                            // printf("Posjetitelj %d: Šaljem odgođeni R-A odgovor za %d\n", data->id, i);
                             deferred_replies[i] = 0;
                         }
                     }
@@ -213,7 +247,9 @@ void *visitor_thread(void *args) {
 void visitor_process(int id) { 
     srand(time(NULL) ^ getpid()); 
     Ci = 0;
+    
     initQueue(&pq);
+    
     memset(deferred_replies, 0, sizeof(deferred_replies));
 
     ThreadData targs;
@@ -267,19 +303,21 @@ void visitor_process(int id) {
             }
         }
         
-        printf("Posjetitelj %d: Poslao R-A zahtjeve (Tm=%d), čekam odgovore...\n", id, Ci);
+        // *** OBRISANO ***: printf("... Poslao R-A zahtjeve ...")
+        // (send_message sada ispisuje 11 pojedinačnih poruka)
+        
+        // Čekaj dok ne dobiješ sve odgovore
         while (rcvd_replies < (NUM_VISITORS - 1)) {
             usleep(10000);
         }
         
         // --- U KRITIČNOM ODSJEČKU ---
-        printf("Posjetitelj %d: Ušao u K.O. Šaljem zahtjev vrtuljku.\n", id);
+        printf("Posjetitelj %d: Ušao u K.O.\n", id);
         
         Message ride_req = {MSG_RIDE, id, Ci, 1};
-        send_message(id, CAROUSEL_ID, &ride_req);
+        send_message(id, CAROUSEL_ID, &ride_req); // Ovo će ispisati "Šaljem ZAHTJEV (Vrtuljak)"
         
-        // *** POPRAVAK: ODMAH IZLAZIMO IZ K.O. ***
-        // (Ovo je Pravilo 4 iz tvog pseudokoda)
+        // --- ODMAH IZLAZIMO IZ K.O. ---
         
         printf("Posjetitelj %d: Izlazim iz K.O. (poslao zahtjev)\n", id);
 
@@ -291,20 +329,19 @@ void visitor_process(int id) {
         }
 
         // 2. Pošalji sve odgođene odgovore
-        // (Ovu logiku premještamo ovdje iz dretve)
         for (int i = 0; i < NUM_VISITORS; i++) {
             if (deferred_replies[i]) {
-                Ci++; // Uskladimo sat prije slanja
+                Ci++; 
                 Message reply = {MSG_REPLY, id, Ci, 0};
-                send_message(id, i, &reply);
-                printf("Posjetitelj %d: Šaljem odgođeni R-A odgovor za %d\n", id, i);
+                send_message(id, i, &reply); // Ovo će ispisati "Šaljem ODGOVOR (R-A)"
+                
+                // *** OBRISANO ***: printf("... Šaljem odgođeni R-A odgovor ...")
+                
                 deferred_replies[i] = 0;
             }
         }
         
         // --- KRAJ KRITIČNOG ODSJEČKA ---
-        
-        // *** POPRAVAK: Sada čekamo vožnju, ALI IZVAN K.O. ***
         
         printf("Posjetitelj %d: Čekam da sjednem...\n", id);
         while (taken == 0) usleep(10000);
@@ -315,9 +352,9 @@ void visitor_process(int id) {
         printf("Posjetitelj %d: Vožnja %d gotova.\n", id, rides);
     }
     
-    printf("Posjetitelj %d: Završio sve vožnje. Šaljem poruku vrtuljku.\n", id);
+    printf("Posjetitelj %d: Završio sve vožnje.\n", id);
     Message done_msg = {MSG_DONE, id, Ci, 0};
-    send_message(id, CAROUSEL_ID, &done_msg);
+    send_message(id, CAROUSEL_ID, &done_msg); // Ovo će ispisati "Šaljem GOTOV"
 
     pthread_cancel(thread);
     for(int i=0; i<NUM_VISITORS; i++) if(targs.read_fds[i] != -1) close(targs.read_fds[i]);
@@ -327,7 +364,7 @@ void visitor_process(int id) {
     exit(0);
 }
 
-// --- Proces Vrtuljak (nepromijenjen) ---
+// --- Proces Vrtuljak ---
 void carousel_process() {
     printf("Vrtuljak: Pokrećem se s %d sjedala.\n", CAROUSEL_SEATS);
     srand(time(NULL) ^ getpid());
@@ -335,7 +372,6 @@ void carousel_process() {
     int read_fds[NUM_VISITORS];
     char pipe_name[50];
     
-    // Otvori sve cijevi za ČITANJE od posjetitelja
     for (int i = 0; i < NUM_VISITORS; i++) {
         get_pipe_name(pipe_name, i, CAROUSEL_ID);
         read_fds[i] = open(pipe_name, O_RDONLY | O_NONBLOCK);
@@ -345,52 +381,55 @@ void carousel_process() {
         }
     }
     
-    int waiting_visitors[NUM_VISITORS]; // Red posjetitelja koji čekaju
+    int waiting_visitors[NUM_VISITORS]; 
     int waiting_count = 0;
-    int seated_visitors[CAROUSEL_SEATS]; // Posjetitelji na vožnji
+    int seated_visitors[CAROUSEL_SEATS]; 
     int finished_visitors = 0;
     
     while(finished_visitors < NUM_VISITORS) {
         
-        // 1. Provjeri ima li novih zahtjeva ili poruka o završetku
         Message msg;
         for (int i = 0; i < NUM_VISITORS; i++) {
             if (read_fds[i] != -1) {
                 ssize_t bytes_read = read(read_fds[i], &msg, sizeof(Message));
                 if (bytes_read > 0) {
+                    
+                    // *** NOVI ISPIS ZA PRIMANJE ***
+                    printf("Vrtuljak  : Primio %-18s <- Posjetitelja %d (Tm=%d)\n", 
+                           msg_type_to_string(msg.type), msg.sender_id, msg.Tm);
+                    // *** KRAJ NOVOG ISPISA ***
+
                     if (msg.type == MSG_RIDE) {
-                        printf("Vrtuljak: Primio zahtjev za vožnju od %d\n", msg.sender_id);
+                        // OBRISAN dupli printf
                         waiting_visitors[waiting_count++] = msg.sender_id;
                     } else if (msg.type == MSG_DONE) {
-                        printf("Vrtuljak: Posjetitelj %d je gotov sa svim vožnjama.\n", msg.sender_id);
+                        // OBRISAN dupli printf
                         finished_visitors++;
-                        close(read_fds[i]); // Zatvori cijev, više nam ne treba
+                        close(read_fds[i]); 
                         read_fds[i] = -1;
                     }
                 }
             }
         }
         
-        // 2. Ako je vrtuljak pun, pokreni vožnju
         if (waiting_count >= CAROUSEL_SEATS) {
-            printf("Vrtuljak: Imam 4 posjetitelja, pokrećem ukrcaj.\n");
+            printf("Vrtuljak: Imam %d putnika, pokrećem ukrcaj.\n", waiting_count);
             
-            // Pošalji poruke "Sjedi" za prva 4
+            // Pošalji poruke "Sjedi"
             for (int i = 0; i < CAROUSEL_SEATS; i++) {
                 int visitor_id = waiting_visitors[i];
                 seated_visitors[i] = visitor_id;
-                Message seat_msg = {MSG_SJEDNI, CAROUSEL_ID, 0, 0};
+                // Vrijeme nije bitno za ovu poruku, ali Ci vrtuljka bi se mogao pratiti
+                Message seat_msg = {MSG_SJEDNI, CAROUSEL_ID, 0, 0}; 
                 send_message(CAROUSEL_ID, visitor_id, &seat_msg);
             }
-            // Pomakni ostatak u redu čekanja
             waiting_count -= CAROUSEL_SEATS;
             for(int i=0; i < waiting_count; i++) {
                 waiting_visitors[i] = waiting_visitors[i + CAROUSEL_SEATS];
             }
             
-            // Pokreni vrtuljak
             printf("Vrtuljak: Pokrenuo vrtuljak.\n");
-            usleep((rand() % 2000 + 1000) * 1000); // Spavaj 1-3 sekunde
+            usleep((rand() % 2000 + 1000) * 1000); 
             printf("Vrtuljak: Zaustavljen.\n");
             
             // Pošalji poruke "Ustani"
@@ -401,7 +440,7 @@ void carousel_process() {
             printf("Vrtuljak: Poslao poruke za ustajanje. Čekam nove posjetitelje.\n");
         }
         
-        usleep(100000); // Pauza da ne vrtimo 100% CPU
+        usleep(100000); 
     }
     
     printf("Vrtuljak: Svi posjetitelji su završili. Zatvaram.\n");
